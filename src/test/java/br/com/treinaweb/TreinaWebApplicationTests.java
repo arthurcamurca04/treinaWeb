e532ea90729810a52af4fff9997103b11df0956d
Arthur <arthur.04.pb@gmail.com>
2019-07-10 20:36:50 -0300

    comite do projeto

---
+++ src/test/java/br/com/treinaweb/TreinaWebApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.treinaweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TreinaWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
